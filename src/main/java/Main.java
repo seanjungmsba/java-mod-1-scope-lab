ab0787e70f176e1ccbc28c530d8c2ad90946d1da
seanjungmsba <seanjeong1996@gmail.com>
2022-06-22 17:00:34 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         // your code here
+
+        boolean flg = false;
+        int num;
+        if (flg) {
+            // do something when flg is true
+            num = 12;
+        } else {
+            // do something else when flg is false
+            num = 5;
+        }
+        System.out.println(num);
     }
 }
